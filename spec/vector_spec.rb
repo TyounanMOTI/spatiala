1f580d48680e57629193ea9ba35ddff192aae773
TyounanMOTI <tyounan.moti@gmail.com>
2010-07-04 14:52:17 +0900

    append 'should' 'when'

--- spec/vector_spec.rb
+++ spec/vector_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Vector do
-  it "returns Vector if initialize with no arguments" do
+  it "should returns Vector when initialize with no arguments" do
     vector = Vector.new()
     vector.should be_instance_of Vector
   end
