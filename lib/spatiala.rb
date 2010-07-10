fdd4dae0866ce0aa89371879c537a1ee7fe49bd1
TyounanMOTI <tyounan.moti@gmail.com>
2010-07-10 15:26:22 +0900

    draw geometry

--- lib/spatiala.rb
+++ lib/spatiala.rb
@@ -1,6 +1,25 @@
-def setup
-  size 640, 640
+require 'geometry'
+require 'ray'
+require 'vector'
+
+class Spatiala < Processing::App
+  def setup
+    size 640, 640
+    smooth
+
+    geometry = Geometry.new(Vector.new(10,20),
+                            Vector.new(400,50),
+                            Vector.new(30,420))
+
+    draw_geometry(geometry)
+  end
+
+  def draw
+  end
+
+  def draw_geometry(geometry)
+    geometry.lines.each { |i| line(i.origin.x, i.origin.y, i.destination.x, i.destination.y)}
+  end
 end
 
-def draw
-end
+Spatiala.new
