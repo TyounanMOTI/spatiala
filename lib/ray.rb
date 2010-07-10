cacde79cad3679604208c630a9fa843c70a0bd61
TyounanMOTI <tyounan.moti@gmail.com>
2010-07-10 15:17:18 +0900

    enable to access Ray.destination

--- lib/ray.rb
+++ lib/ray.rb
@@ -1,8 +1,9 @@
 class Ray
-  attr_accessor :origin, :delta
+  attr_accessor :origin, :destination, :delta
 
   def initialize(origin=Vector.new(0,0), destination=Vector.new(0,0))
     @origin = origin
+    @destination = destination
     @delta = destination - origin
   end
 end
--- spec/ray_spec.rb
+++ spec/ray_spec.rb
@@ -5,9 +5,10 @@ describe Ray do
     Ray.new.should be_instance_of Ray
   end
 
-  it "should origin=(3,4), delta=(-9,-4) when initialize in that origin, destination=(-6,0)" do
+  it "should origin=(3,4), destination(-6,0), delta=(-9,-4) when initialize in that origin, destination=(-6,0)" do
     ray = Ray.new(Vector.new(3,4), Vector.new(-6,0))
     ray.origin.should == Vector.new(3,4)
     ray.delta.should == Vector.new(-9,-4)
+    ray.destination.should == Vector.new(-6,0)
   end
 end
