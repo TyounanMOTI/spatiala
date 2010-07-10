f15d54d7b0d892ec3ec05724f8225bf90a77c269
TyounanMOTI <tyounan.moti@gmail.com>
2010-07-10 14:57:52 +0900

    create Geometry class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'vector'
 require 'ray'
+require 'geometry'
 require 'spec'
 require 'spec/autorun'
 
