2020c7cd20ed99f30e2c5d213697b1e697a128a2
TyounanMOTI <tyounan.moti@gmail.com>
2010-07-10 18:31:48 +0900

    add position, direction member in Listener

--- lib/listener.rb
+++ lib/listener.rb
@@ -1,4 +1,8 @@
 class Listener
+  attr_accessor :position, :direction
+
   def initialize(position, direction)
+    @position = position
+    @direction = direction.normalize
   end
 end
--- spec/listener_spec.rb
+++ spec/listener_spec.rb
@@ -1,7 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Listener do
+  before do
+    @listener = Listener.new(Vector.new(10,10), Vector.new(40,50).normalize)
+  end
+
   it "should be initialized with position, direction" do
-    Listener.new(Vector.new(10,10), Vector.new(40,50).normalize).should be_instance_of Listener
+    @listener.should be_instance_of Listener
+  end
+
+  it "should have position, direction" do
+    @listener.position.should == Vector.new(10,10)
+    @listener.direction.should == Vector.new(40,50).normalize
   end
 end
