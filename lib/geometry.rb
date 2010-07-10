22e588cdb7c417a5cbb9879ab5b9d8360fbd027d
TyounanMOTI <tyounan.moti@gmail.com>
2010-07-10 15:07:33 +0900

    enable to fetch lines in Geometry

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -1,2 +1,11 @@
 class Geometry
+  def initialize(*vertex)
+    @vertex = vertex
+  end
+
+  def lines
+    result = Array.new
+    @vertex.each_index { |i| result.push(Ray.new(@vertex[i], @vertex.fetch(i+1, @vertex[0])))}
+    return result
+  end
 end
