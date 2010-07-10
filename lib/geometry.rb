c1ef6d59a5d3a48f3e86307e6f361b192cf794de
TyounanMOTI <tyounan.moti@gmail.com>
2010-07-10 16:46:38 +0900

    initialize Geometry with including Polygons

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -1,4 +1,12 @@
 require 'polygon'
 
 class Geometry
+  attr_reader :polygons
+
+  def initialize
+  end
+
+  def initialize(*polygons)
+    @polygons = polygons
+  end
 end
--- spec/geometry_spec.rb
+++ spec/geometry_spec.rb
@@ -1,7 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Geometry do
+  before do
+    triangle = Polygon.new(Vector.new(10,20),
+                           Vector.new(400,50),
+                           Vector.new(30,420))
+    rectangle = Polygon.new(Vector.new(50,30),
+                            Vector.new(100,40),
+                            Vector.new(100,400),
+                            Vector.new(60,300))
+    @geometry = Geometry.new(triangle, rectangle)
+  end
+
   it "should generate instance" do
     Geometry.new.should be_instance_of Geometry
+  end
+
+  it "should be initialized by including polygons" do
+    @geometry.should be_instance_of Geometry
+  end
+
+  it "should have including polygons" do
+    @geometry.polygons.each { |i| i.should be_instance_of Polygon }
   end
 end
