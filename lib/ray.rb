701c970f6a9e1f7c015e6286f959670a4ea94973
TyounanMOTI <tyounan.moti@gmail.com>
2010-07-10 14:51:52 +0900

    initialize Ray with origin, destination.

--- lib/ray.rb
+++ lib/ray.rb
@@ -1,2 +1,8 @@
 class Ray
+  attr_accessor :origin, :delta
+
+  def initialize(origin=Vector.new(0,0), destination=Vector.new(0,0))
+    @origin = origin
+    @delta = destination - origin
+  end
 end
--- spec/ray_spec.rb
+++ spec/ray_spec.rb
@@ -4,4 +4,10 @@ describe Ray do
   it "should generate instance" do
     Ray.new.should be_instance_of Ray
   end
+
+  it "should origin=(3,4), delta=(-9,-4) when initialize in that origin, destination=(-6,0)" do
+    ray = Ray.new(Vector.new(3,4), Vector.new(-6,0))
+    ray.origin.should == Vector.new(3,4)
+    ray.delta.should == Vector.new(-9,-4)
+  end
 end
