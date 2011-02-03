2bd60b69b1f8df87d19afb34161c020649a8de18
TyounanMOTI <tyounan.moti@gmail.com>
2011-02-03 18:31:58 +0900

    refactor spec of Listener

--- spec/listener_spec.rb
+++ spec/listener_spec.rb
@@ -1,25 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Listener do
-  before do
-    @listener = Listener.new(Vector.new(10,10), Vector.new(40,50).normalize)
-  end
+  let(:listener) { Listener.new(Vector.new(10,10), Vector.new(40,50).normalize) }
 
-  it "should be initialized with position, direction" do
-    @listener.should be_instance_of Listener
-  end
+  subject { listener }
 
-  it "should have position, direction" do
-    @listener.position.should == Vector.new(10,10)
-    @listener.direction.should == Vector.new(40,50).normalize
-  end
+  it { should be_instance_of Listener }
+  its(:position) { should == Vector.new(10,10) }
+  its(:direction) { should == Vector.new(40,50).normalize }
 
-  it "should have children array" do
-    @listener.children.should be_instance_of Array
-  end
-
-  it "should return Listener when transformed" do
-    @listener.transform(Matrix::Translator[2,3]).should be_instance_of Listener
+  describe "#transform" do
+    subject { listener.transform(Matrix::Translator[2,3]) }
+    it { should be_a Listener }
+    its(:position) { should == Vector.new(12,13) }
   end
 end
 
